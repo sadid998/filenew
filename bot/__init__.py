77cf92316c9697c7b9b539e590a6e856c5e3beb0
sadid998 <75202585+sadid998@users.noreply.github.com>
2022-02-22 23:54:51 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -61,7 +61,7 @@ async def download(event):
             bot_url = f"t.me/{username_bot}?start={id_hex}"
             response = requests.get(f"https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id}").text
             linkk = (json.loads(response)["shortenedUrl"])
-            await event.reply(f"Link to download file: \n\n📎 : {linkk} \n {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
+            await event.reply(f"Link to download file: \n\n📎 : {linkk}\n\n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -82,12 +82,14 @@ async def download(event):
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
                         bot_url = f"t.me/{username_bot}?start={id_hex}"
-                        forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
+responseo = requests.get(f"https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id_name}").text
+            linkks = (json.loads(responseo)["shortenedUrl"])
+                        forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {linkks}\n\n🤖 : {bot_url}",link_preview=False)
                         await asyncio.sleep(12)
-                        await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
+                        await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {linkks}\n\n🤖 : {bot_url}")
                         await asyncio.sleep(10)
                         await forward.delete()
-                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
+                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {linkks}",link_preview=True)
                 return
         
         if pv:
