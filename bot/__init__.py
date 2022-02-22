d205de034b5afa2b95896cd40a61ea67deffb74b
sadid998 <75202585+sadid998@users.noreply.github.com>
2022-02-22 17:20:51 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,6 +6,7 @@ import asyncio
 import threading
 import requests
 import re
+import json
 from urllib.parse import quote
 
 def cronjob():
@@ -56,7 +57,8 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
+   response = requests.get("https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id}").json()["shortenedUrl"]
+            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n {response} \n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
