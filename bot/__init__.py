da3cc55da0dcfc40c39b689b846ce00b89fd1e1e
sadid998 <75202585+sadid998@users.noreply.github.com>
2022-02-23 20:42:05 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -58,9 +58,8 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            response = requests.get(f"https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id}").text
-            linkk = (json.loads(response)["shortenedUrl"])
-            await event.reply(f"Link to download file: \n\n📎 : {linkk}\n\n🤖 : {bot_url}")
+            
+            await event.reply(f"Link to download file: \n\n📎 : {id}\n\n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -81,6 +80,8 @@ async def download(event):
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
                         bot_url = f"t.me/{username_bot}?start={id_hex}" 
+response = requests.get(f"https://droplink.co/api?api=0b8645cc45284e92d722e59c60d918291526a16c&url={Config.DOMAIN}/{id}").text
+            linkk = (json.loads(response)["shortenedUrl"])
                         forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}",link_preview=False)
                         await asyncio.sleep(12)
                         await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {linkk}\n\n🤖 : {bot_url}")
